7833c671e4dfb0b180cc6b9f01d24ae47f8229e4
Gustavo De la Cruz Tovar <gusdelact@Gustavo-De-la-Cruz-Tovars-MacBook.local>
2018-06-07 14:36:40 -0500

    se estructuro el código de la aplicacion principal

--- src/main/java/com/gfi/microservicios/BusinessruleserviceApplication.java
+++ src/main/java/com/gfi/microservicios/BusinessruleserviceApplication.java
@@ -1,6 +1,9 @@
 package com.gfi.microservicios;
 
+import java.io.PrintStream;
+
 import org.kie.api.KieServices;
+import org.kie.api.cdi.KSession;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.boot.SpringApplication;
@@ -11,22 +14,31 @@ import com.gfi.microservicios.model.Item;
 @SpringBootApplication
 public class BusinessruleserviceApplication {
 
+
+
+	private KieSession kSession;
+
+	public void boot() {
+		System.out.println( "Bootstrapping the Rule Engine ..." );
+		//1) Bootstrapping a Rule Engine Session
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		this.kSession =  kContainer.newKieSession();
+	}
+	public  void go(PrintStream out){
+		Item item = new Item("A", 123.0f,234.0f);
+		out.println( "Item Category: " + item.getCategory());
+		kSession.insert(item);
+		int fired = kSession.fireAllRules();
+		out.println( "Number of Rules executed = " + fired );
+		out.println( "Item Category: " + item.getCategory());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessruleserviceApplication.class, args);
-		
-		 System.out.println( "Bootstrapping the Rule Engine ..." );
-		    //1) Bootstrapping a Rule Engine Session
-		    KieServices ks = KieServices.Factory.get();
-		    KieContainer kContainer = ks.getKieClasspathContainer();
-		    KieSession kSession =  kContainer.newKieSession();
-		        
-		    Item item = new Item("A", 123.0f,234.0f);  
-		    System.out.println( "Item Category: " + item.getCategory());
-		    //2) Provide information to the Rule Engine Context
-		    kSession.insert(item);
-		    //3) Execute the rules that are matching
-		    int fired = kSession.fireAllRules();
-		    System.out.println( "Number of Rules executed = " + fired );
-		    System.out.println( "Item Category: " + item.getCategory());
+
+		BusinessruleserviceApplication bsrApp = new BusinessruleserviceApplication() ;
+		bsrApp.boot();
+		bsrApp.go(System.out);
 	}
 }
